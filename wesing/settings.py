bd06c38cc19550d6f90f35896ca06c699dddccb8
jinho1011 <gino9940@gmail.com>
2020-07-20 19:45:35 +0900

    Create song application

--- wesing/settings.py
+++ wesing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u9&29s4)$2mp$$#*ghwm^8@12p#b7p3=*w(x9s47v4gx7i)1dv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+INSTALLED_APPS += [
+    'song'
 ]
 
 MIDDLEWARE = [
